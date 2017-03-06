91d865566f9843ba6c862606e8f775a545fe395c
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-06 14:15:50 +0000

    Update Transaction class with Account Balance and Transaction Amount
    When a new transaction is created, it takes transaction amount and account balance as arguments and stores them as instances of the class.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction
+  attr_reader :transaction_amount, :account_balance
+
+  def initialize(balance, amount)
+    @account_balance = balance
+    @transaction_amount = amount
+  end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,17 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { Transaction.new }
+  let(:account_balance) { 100 }
+  let(:transaction_amount) { 20 }
+  subject(:transaction) { Transaction.new(account_balance, transaction_amount) }
 
   describe "::new" do
-    it "creates a new transaction" do
-      expect(transaction).to be_instance_of(Transaction)
+    it "Creates a new transaction with the transaction amount" do
+      expect(transaction.transaction_amount).to eq(transaction_amount)
+    end
+
+    it "Creates a new transaction with the updated account balance" do
+      expect(transaction.account_balance).to eq(account_balance)
     end
   end
 end
