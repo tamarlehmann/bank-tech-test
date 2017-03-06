d9812335dc721a71fac15fc24fe424fd495091a9
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-06 14:42:22 +0000

    Update Transaction class to store the transaction type

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction
-  attr_reader :transaction_amount, :account_balance, :date
+  attr_reader :transaction_amount, :account_balance, :date, :transaction_type
 
-  def initialize(balance, amount, date = Time.now.strftime("%d/%m/%Y"))
+  def initialize(balance, amount, date = Time.now.strftime("%d/%m/%Y"), type = nil)
     @account_balance = balance
     @transaction_amount = amount
     @date = date
+    @transaction_type = type
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,7 +4,8 @@ describe Transaction do
   let(:account_balance) { 100 }
   let(:transaction_amount) { 20 }
   let(:transaction_date) { "06/03/2017" }
-  subject(:transaction) { Transaction.new(account_balance, transaction_amount, transaction_date) }
+  let(:transaction_type) { nil }
+  subject(:transaction) { Transaction.new(account_balance, transaction_amount, transaction_date, transaction_type) }
 
   describe "::new" do
     it "Creates a new transaction with the transaction amount" do
@@ -17,6 +18,10 @@ describe Transaction do
 
     it "Stores the associated transaction date with each transaction" do
       expect(transaction.date).to eq(transaction_date)
+    end
+
+    it "Creates a new transaction with an un-set transaction type" do
+      expect(transaction.transaction_type).to eq(transaction_type)
     end
   end
 end
