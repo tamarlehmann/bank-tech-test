c5bdb4d82c082d1ade44b6de8cfde34c1550cf81
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-06 15:40:26 +0000

    Update BankAccount to store a transaction history
    Updated bank account to be initialized with an empty transaction history. Added a method that is run after each deposit/withdrawal so that the transaction is added to the transaction history.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,26 +1,34 @@
 require_relative 'transaction'
 
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :transaction_history
   MIN_BALANCE = 0
 
-  def initialize(balance)
+  def initialize(balance, history = [])
     @balance = balance
+    @transaction_history = history
   end
 
   def deposit(amount)
     @balance += amount
-    transaction = Transaction.new(@balance, amount)
-    transaction.credit_transaction
+    @transaction = Transaction.new(@balance, amount)
+    @transaction.credit_transaction
+    complete_transaction
   end
 
   def withdrawal(amount)
     message = "You have insufficient funds, the maximum you can withdraw is #{balance}"
     raise message if (balance - amount) < MIN_BALANCE
     @balance -= amount
-    transaction = Transaction.new(@balance, amount)
-    transaction.debit_transaction
-    p transaction
+    @transaction = Transaction.new(@balance, amount)
+    @transaction.debit_transaction
+    complete_transaction
+  end
+
+  private
+
+  def complete_transaction
+    @transaction_history.push(@transaction)
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,11 +2,16 @@ require 'bank_account'
 
 describe BankAccount do
   let(:balance) { 0 }
-  subject(:account) { BankAccount.new(balance) }
+  let(:history) { [] }
+  subject(:account) { BankAccount.new(balance, history) }
 
   describe "::new" do
     it "Creates a new bank account with a balance of 0" do
       expect(account.balance).to eq(balance)
+    end
+
+    it "Creates a new bank account with an empty transaction history" do
+      expect(account.transaction_history).to be_empty
     end
   end
 
@@ -15,6 +20,11 @@ describe BankAccount do
 
     it "Increases account balance with value of the deposit" do
       expect{ account.deposit(deposit_amount) }.to change{ account.balance }.by(deposit_amount)
+    end
+
+    it "Stores the transaction in the Transaction History" do
+      account.deposit(deposit_amount)
+      expect(account.transaction_history.length).to eq(1)
     end
   end
 
@@ -34,6 +44,11 @@ describe BankAccount do
     it "Does not allow withdrawal when there are insufficient funds" do
       message = "You have insufficient funds, the maximum you can withdraw is #{account.balance}"
       expect{ account.withdrawal(large_withdrawal_amount)}.to raise_error(message)
+    end
+
+    it "Stores the transaction in the Transaction History" do
+      account.withdrawal(withdrawal_amount)
+      expect(account.transaction_history.length).to eq(2)
     end
   end
 end
