a8a473a4b9da88e208e5e0d163b25c4ea3714c9f
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-07 11:11:25 +0000

    Create Statement Printer using history

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -4,5 +4,34 @@ class BankStatement
   def initialize(history)
     @contents = history
   end
-  
+
+  def print_statement
+    print_header
+    print_body
+  end
+
+  private
+
+  def print_header
+    header = ""
+    header += "Date".ljust(11) + "||"
+    header += "Credit".ljust(8) + "||"
+    header += "Debit".ljust(8) + "||"
+    header += "Balance\n".ljust(8)
+    puts header
+  end
+
+  def print_body
+    row_text = ""
+    @contents.reverse.each do |transaction|
+      row_text += "#{transaction.date}".ljust(11) + "||"
+      if transaction.transaction_type == "credit"
+        row_text += "#{transaction.transaction_amount}".ljust(8) + "||" + " ".ljust(8) + "||"
+      else
+        row_text += " ".ljust(8) + "||" + "#{transaction.transaction_amount}".ljust(8) + "||"
+      end
+      row_text += "#{transaction.account_balance}".ljust(8) + "\n"
+    end
+    puts row_text
+  end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,13 +1,37 @@
 require 'bank_statement'
 
 describe BankStatement do
-  subject(:history) { double :history }
-  subject(:statement) { BankStatement.new(history) }
+    let(:transaction_history) { double :transaction_history }
+    let(:print_body) { "07/03/2017 ||50      ||        ||50" }
+    let(:print_header) { "Date       ||Credit  ||Debit   ||Balance" }
+    subject(:statement) { BankStatement.new(transaction_history) }
 
 
   describe "::new" do
     it "Creates a new statement with transaction history to be printed" do
-      expect(statement.contents).to eq(history)
+      expect(statement.contents).to eq(transaction_history)
+    end
+  end
+
+  describe "#print_statement" do
+    before do
+      allow(statement).to receive(:print_statement).and_return(print_header + print_body)
+    end
+
+    it "Prints bank statement body" do
+      body = "07/03/2017 ||50      ||        ||50"
+      expect(statement.print_statement).to include(body)
+    end
+
+    it "Prints bank statement header" do
+      header = "Date       ||Credit  ||Debit   ||Balance"
+      expect(statement.print_statement).to include(header)
+    end
+
+    it "Prints whole bank statement" do
+      header = "Date       ||Credit  ||Debit   ||Balance"
+      body = "07/03/2017 ||50      ||        ||50"
+      expect(statement.print_statement).to eq(header + body)
     end
   end
 end
